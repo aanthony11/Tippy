ab048822961a4b32cd8152486edbbb4166c4e913
austin <austinanthony226@yahoo.com>
2020-01-23 23:28:29 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Austin Anthony on 1/23/20.
+//  Copyright © 2020 Austin Anthony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
